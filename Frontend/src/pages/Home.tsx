29d7c72acfc1ce16882b9b5d9f1fac78d519792e
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2025-09-09 20:50:36 +0900

    feat: HomeコンポーネントにPOSTリクエスト機能を追加し、レスポンスを表示

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+
 function Home() {
-  return <div>Home</div>;
+  const [response, setResponse] = useState(null);
+  const handlePostRequest = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      }
+    })
+    const data = await response.json()
+    setResponse(data)
+  }
+
+  useEffect(() => {
+    handlePostRequest()
+  }, []);
+
+  return <>
+    <div>
+      Home
+    </div>
+    <div>
+      {JSON.stringify(response, null, 2)}
+    </div>
+  </>;
 }
 
 export default Home;
