7bea65a0ef261f502bd938ec913f7ea9c9bf3574
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2025-09-21 16:24:34 +0900

    update App.tsx

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,20 +1,30 @@
 import ViteDefault from "./pages/ViteDefault"
 import PostRequest from "./pages/PostRequest"
 import ComponentVSFunction from "./pages/ComponentVSFunction"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Link } from "react-router-dom"
 
 function App() {
 
   return (
     <>
+      <nav>
+        <ul style={{ display: 'flex', listStyle: 'none', padding: 0 }}>
+          <li style={{ margin: '0 10px' }}>
+            <Link to="/">ViteDefault</Link>
+          </li>
+          <li style={{ margin: '0 10px' }}>
+            <Link to="/post-request">PostRequest</Link>
+          </li>
+          <li style={{ margin: '0 10px' }}>
+            <Link to="/component-vs-function">ComponentVSFunction</Link>
+          </li>
+        </ul>
+      </nav>
+
       <Routes>
-        <Route path="/" element={
-          <>
-            <ViteDefault />
-            <PostRequest />
-            <ComponentVSFunction />
-          </>
-        } />
+        <Route path="/" element={<ViteDefault />} />
+        <Route path="/post-request" element={<PostRequest />} />
+        <Route path="/component-vs-function" element={<ComponentVSFunction />} />
       </Routes>
     </>
   )
