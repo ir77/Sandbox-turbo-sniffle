44da85b833d0ef1413525c0fb59d000513aa7bf0
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2025-09-21 16:51:23 +0900

    update App.tsx

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,27 +1,39 @@
-import ViteDefault from "./pages/ViteDefault"
-import PostRequest from "./pages/PostRequest"
-import ComponentVSFunction from "./pages/ComponentVSFunction"
-import Wip from "./pages/wip"
-import { Routes, Route, NavLink } from "react-router-dom"
+import ViteDefault from "./pages/ViteDefault";
+import PostRequest from "./pages/PostRequest";
+import ComponentVSFunction from "./pages/ComponentVSFunction";
+import Wip from "./pages/wip";
+import { Routes, Route, NavLink } from "react-router-dom";
+import React from "react";
+
+type NavigationItemProps = {
+  to: string;
+  children: React.ReactNode;
+};
+
+const NavigationItem = ({ to, children }: NavigationItemProps) => {
+  const navLinkClassName = ({ isActive }: { isActive: boolean }) =>
+    isActive ? "active" : "";
+
+  return (
+    <li>
+      <NavLink to={to} className={navLinkClassName}>
+        {children}
+      </NavLink>
+    </li>
+  );
+};
 
 function App() {
-
   return (
     <>
       <nav className="main-nav">
         <ul>
-          <li>
-            <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>WIP</NavLink>
-          </li>
-          <li>
-            <NavLink to="/post-request" className={({ isActive }) => isActive ? "active" : ""}>PostRequest</NavLink>
-          </li>
-          <li>
-            <NavLink to="/component-vs-function" className={({ isActive }) => isActive ? "active" : ""}>ComponentVSFunction</NavLink>
-          </li>
-          <li>
-            <NavLink to="/vite-default" className={({ isActive }) => isActive ? "active" : ""}>ViteDefault</NavLink>
-          </li>
+          <NavigationItem to="/">WIP</NavigationItem>
+          <NavigationItem to="/post-request">PostRequest</NavigationItem>
+          <NavigationItem to="/component-vs-function">
+            ComponentVSFunction
+          </NavigationItem>
+          <NavigationItem to="/vite-default">ViteDefault</NavigationItem>
         </ul>
       </nav>
 
@@ -30,12 +42,15 @@ function App() {
           <Route path="/" element={<Wip />} />
           <Route path="/wip" element={<Wip />} />
           <Route path="/post-request" element={<PostRequest />} />
-          <Route path="/component-vs-function" element={<ComponentVSFunction />} />
+          <Route
+            path="/component-vs-function"
+            element={<ComponentVSFunction />}
+          />
           <Route path="/vite-default" element={<ViteDefault />} />
         </Routes>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
