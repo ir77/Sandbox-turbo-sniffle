0e0b592302f4172da86e1018f94ca7af655cb156
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2025-09-21 16:40:05 +0900

    update css

--- Frontend/src/pages/ViteDefault.tsx
+++ Frontend/src/pages/ViteDefault.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
-import './ViteDefault.css'
+import styles from './ViteDefault.module.css'
 
 function ViteDefault() {
       const [count, setCount] = useState(0)
@@ -10,14 +10,14 @@ function ViteDefault() {
         <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className={styles.logo} alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className={`${styles.logo} ${styles.react}`} alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className={styles.card}>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,7 +25,7 @@ function ViteDefault() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className={styles['read-the-docs']}>
         Click on the Vite and React logos to learn more
       </p>
     </>
