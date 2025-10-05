1f87cc8df860ba3ee69b4de26b5b35e51b009efe
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2025-10-05 20:41:47 +0900

    feat: Cardコンポーネントを追加し、ComponentVSFunctionで使用

--- Frontend/src/pages/ComponentVSFunction.tsx
+++ Frontend/src/pages/ComponentVSFunction.tsx
@@ -1,5 +1,14 @@
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 function ComponentSample() {
   const [count, setCount] = useState(0);
@@ -16,23 +25,33 @@ function ComponentVSFunction() {
 
   return (
     <>
-      <h1>Component VS Function</h1>
-      <h2>ComponentSample as Component</h2>
-      <ol>
-        <li><button onClick={() => setShowFirst(!showFirst)}>Toggle First</button></li>
-        <li>{showFirst && <ComponentSample />}</li>
-        <li><ComponentSample /></li>
-      </ol>
-      <h2>ComponentSample as Function</h2>
-      <ol>
-
-        {/* Uncaught Error: Rendered fewer hooks than expected.*/}
-        {/* 親コンポーネントにhooksが登録されてしまい、要素が消えるとクラッシュしてしまう。 */}
-        <li><button onClick={() => setShowSecond(!showSecond)}>Toggle Second Cause Crash!</button></li>
-        <li>{showSecond && ComponentSample()}</li>
-        {/* ↑↓の独立性は保たれる */}
-        <li>{ComponentSample()}</li>
-      </ol>
+      <Card>
+        <CardHeader>
+          <CardTitle>ComponentSample as Component</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ol>
+            <li><Button onClick={() => setShowFirst(!showFirst)}>Toggle First</Button></li>
+            <li>{showFirst && <ComponentSample />}</li>
+            <li><ComponentSample /></li>
+          </ol>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>ComponentSample as Function</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ol>
+            {/* Uncaught Error: Rendered fewer hooks than expected.*/}
+            {/* 親コンポーネントにhooksが登録されてしまい、要素が消えるとクラッシュしてしまう。 */}
+            <li><Button onClick={() => setShowSecond(!showSecond)}>Toggle Second Cause Crash!</Button></li>
+            <li>{showSecond && ComponentSample()}</li>
+            {/* ↑↓の独立性は保たれる */}
+            <li>{ComponentSample()}</li>
+          </ol>
+        </CardContent>
+      </Card>
     </>
   )
 }
